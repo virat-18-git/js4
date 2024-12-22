371bd8334a5549abb01dc43ae502ce97ed40a48c
virat-18-git <ksprddy6@gmail.com>
2024-12-22 19:44:40 +0530

    operators1

---
+++ index.js
@@ -0,0 +1,155 @@
+// ADDITION
+
+// let a=123;
+// b=456;
+// c=a+b;
+// console.log(c)
+
+// let arr=[45,25,68,89,65,32]
+// sum=arr[0]+arr[arr.length-1]
+// console.log(sum)
+
+// let obj={
+//     e_id:"456",
+//     e_name:"shiva",
+//     e_netpay:55000,
+//     e_tax:5000,
+// }
+// let gross_pay=obj.e_netpay+obj.e_tax
+// let gross_pay=obj["e_netpay"]+obj["e_tax"]
+// console.log(gross_pay)
+
+
+// subtraction
+
+let a=25;
+b=15;
+// console.log(a-b)
+// console.log(b-a)
+
+// multipliaction
+// console.log(a*b)
+
+// exponential
+// console.log(a**b)
+
+// division
+// console.log(a/b) prints quotient value
+
+// modulus
+// console.log(a%b) prints remainder value
+
+
+
+// increment and decrement
+// let x=5;
+// console.log(++x)
+// x++;
+// console.log(x++)
+// console.log(x)
+// console.log(++x)
+// console.log(x--)
+// console.log(--x)
+
+
+
+// assignment operators;
+
+// additional assignment operator
+// let x=50;
+// x=x+50;
+// x+=50
+// console.log(x)
+
+// subt. assignment operator
+// x=x-50;
+// x-=50
+// console.log(x)
+
+
+// division assignment operator
+let x=100;
+// x=x/5;
+// x/=5
+// console.log(x)
+
+
+// modulus assignmenet operator
+// x=x%5;
+// x%=5
+// console.log(x)
+
+
+// exponential assignmenet operator
+// x=x**5;
+// x**=5
+// console.log(x)
+
+
+// multiplication assignmenet operator
+// x=x*5;
+// x*=5
+// console.log(x)
+
+// comparition operators
+// let s1=10;
+// s2=20;
+// console.log(s1==s2)
+
+
+// let s1=10;
+// s2=20;
+// console.log(s1!=s2)
+
+// let s1="10";
+// s2=20;
+// console.log(s1===s2)
+
+// let s1="10";
+// s2=10;
+// console.log(s1!==s2)
+
+
+// string operators
+// let y1="25";
+// y2=+y1;
+// console.log(typeof y1)
+// console.log(typeof y2)
+
+// let str1="shiva ";concatenation
+// str2="prasad";
+// console.log(str1+str2)
+
+// let str1="123";
+// str2=456;
+// console.log(str1+str2)
+
+// let str="kamal ";   concatenation assignment
+// str+="hassan";  
+// console.log(str)
+
+
+let arr=["hiiii ","hlooo ","welcome "]
+let ip="shiva";
+console.log(arr[0]+ip,arr[1]+ip,arr[2]+ip,)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
